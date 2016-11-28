3fa43162bf3880115f59f13665eb8f940f22ebc1
Francis Yan <francisyyan@gmail.com>
2016-11-27 23:07:16 -0800

    tcp: instrument how long TCP is limited by insufficient send buffer
    This patch measures the amount of time when TCP runs out of new data
    to send to the network due to insufficient send buffer, while TCP
    is still busy delivering (i.e. write queue is not empty). The goal
    is to indicate either the send buffer autotuning or user SO_SNDBUF
    setting has resulted network under-utilization.
    
    The measurement starts conservatively by checking various conditions
    to minimize false claims (i.e. under-estimation is more likely).
    The measurement stops when the SOCK_NOSPACE flag is cleared. But it
    does not account the time elapsed till the next application write.
    Also the measurement only starts if the sender is still busy sending
    data, s.t. the limit accounted is part of the total busy time.
    
    Signed-off-by: Francis Yan <francisyyan@gmail.com>
    Signed-off-by: Yuchung Cheng <ycheng@google.com>
    Signed-off-by: Soheil Hassas Yeganeh <soheil@google.com>
    Acked-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -5131,8 +5131,11 @@ static void tcp_check_space(struct sock *sk)
 		/* pairs with tcp_poll() */
 		smp_mb();
 		if (sk->sk_socket &&
-		    test_bit(SOCK_NOSPACE, &sk->sk_socket->flags))
+		    test_bit(SOCK_NOSPACE, &sk->sk_socket->flags)) {
 			tcp_new_space(sk);
+			if (!test_bit(SOCK_NOSPACE, &sk->sk_socket->flags))
+				tcp_chrono_stop(sk, TCP_CHRONO_SNDBUF_LIMITED);
+		}
 	}
 }
 
