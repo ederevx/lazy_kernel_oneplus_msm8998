717c65e1d3e5c274002c927ca4bb8fb908eea139
Daniel Axtens <dja@axtens.net>
2017-05-11 06:11:39 +1000

    sched/fair: WARN() and refuse to set buddy when !se->on_rq
    If we set a next or last buddy for a se that is not on_rq, we will
    end up taking a NULL pointer dereference in wakeup_preempt_entity
    via pick_next_task_fair.
    
    Detect when we would be about to do that, throw a warning and
    then refuse to actually set it.
    
    This has been suggested at least twice:
    
      https://marc.info/?l=linux-kernel&m=146651668921468&w=2
      https://lkml.org/lkml/2016/6/16/663
    
    I recently had to debug a problem with these (we hadn't backported
    Konstantin's patches in this area) and this would have saved a lot
    of time/pain.
    
    Just do it.
    
    Signed-off-by: Daniel Axtens <dja@axtens.net>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/20170510201139.16236-1-dja@axtens.net
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7545,8 +7545,11 @@ static void set_last_buddy(struct sched_entity *se)
 	if (entity_is_task(se) && unlikely(task_of(se)->policy == SCHED_IDLE))
 		return;
 
-	for_each_sched_entity(se)
+	for_each_sched_entity(se) {
+		if (SCHED_WARN_ON(!se->on_rq))
+			return;
 		cfs_rq_of(se)->last = se;
+	}
 }
 
 static void set_next_buddy(struct sched_entity *se)
@@ -7554,8 +7557,11 @@ static void set_next_buddy(struct sched_entity *se)
 	if (entity_is_task(se) && unlikely(task_of(se)->policy == SCHED_IDLE))
 		return;
 
-	for_each_sched_entity(se)
+	for_each_sched_entity(se) {
+		if (SCHED_WARN_ON(!se->on_rq))
+			return;
 		cfs_rq_of(se)->next = se;
+	}
 }
 
 static void set_skip_buddy(struct sched_entity *se)
