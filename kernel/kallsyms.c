9dd3b3032b1cfbf311be311ae363247b4f2ffe56
Edrick Vince Sinsuan <sedrickvince@gmail.com>
2020-04-07 20:06:06 +0300

    HACK: kernel: kallsyms: silence warning
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -367,7 +367,8 @@ static int __sprint_symbol(char *buffer, unsigned long address,
 		return sprintf(buffer, "0x%lx", address - symbol_offset);
 
 	if (name != buffer)
-		strcpy(buffer, name);
+		strcpy(strcpy(buffer, ""), name);
+		
 	len = strlen(buffer);
 	offset -= symbol_offset;
 
