f523b9140ce1772caefbd21c9899a99ea5abbe85
ederekun <sedrickvince@gmail.com>
2020-05-05 14:16:38 +0300

    drivers: staging: qcacld-3.0: Use default mac address instead of .bin
    This provides the real/firmware provided mac address that stock gives by
    prioritizing fw provided MAC address over platform driver and
    wlan_mac.bin provided MAC Address. This patch ensures that if ever
    qcacld fails to get fw mac address, it will fallback to platform driver
    and wlan_mac.bin.
    
    Signed-off-by: Edrick Vince Sinsuan <sedrickvince@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -11666,18 +11666,6 @@ static int hdd_initialize_mac_address(struct hdd_context *hdd_ctx)
 	int ret;
 	bool update_mac_addr_to_fw = true;
 
-	ret = hdd_platform_wlan_mac(hdd_ctx);
-	if (hdd_ctx->config->mac_provision || !ret) {
-		hdd_info("using MAC address from platform driver");
-		return ret;
-	}
-
-	status = hdd_update_mac_config(hdd_ctx);
-	if (QDF_IS_STATUS_SUCCESS(status)) {
-		hdd_info("using MAC address from wlan_mac.bin");
-		return 0;
-	}
-
 	hdd_info("using default MAC address");
 
 	/* Use fw provided MAC */
@@ -11685,7 +11673,6 @@ static int hdd_initialize_mac_address(struct hdd_context *hdd_ctx)
 		reverse_byte_array(&hdd_ctx->hw_macaddr.bytes[0], 6);
 		hdd_update_macaddr(hdd_ctx, hdd_ctx->hw_macaddr, false);
 		update_mac_addr_to_fw = false;
-		return 0;
 	} else if (hdd_generate_macaddr_auto(hdd_ctx) != 0) {
 		struct qdf_mac_addr mac_addr;
 
@@ -11701,9 +11688,27 @@ static int hdd_initialize_mac_address(struct hdd_context *hdd_ctx)
 
 	if (update_mac_addr_to_fw) {
 		ret = hdd_update_mac_addr_to_fw(hdd_ctx);
-		if (ret)
-			hdd_err("MAC address out-of-sync, ret:%d", ret);
+		if (!ret) {
+			hdd_info("using fw generated MAC address");
+			return 0;
+		}
+	} else {
+		hdd_info("using fw provided MAC address");
+		return 0;
 	}
+
+	ret = hdd_platform_wlan_mac(hdd_ctx);
+	if (hdd_ctx->config->mac_provision || !ret) {
+		hdd_info("using MAC address from platform driver");
+		return ret;
+	}
+
+	status = hdd_update_mac_config(hdd_ctx);
+	if (QDF_IS_STATUS_SUCCESS(status)) {
+		hdd_info("using MAC address from wlan_mac.bin");
+		return 0;
+	}
+
 	return ret;
 }
 
