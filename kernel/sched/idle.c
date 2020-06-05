d38a1708dd4257ff286d3b12c975b5b6813d1c07
ederekun <sedrickvince@gmail.com>
2020-06-05 18:57:13 +0300

    Revert "sched/idle: Optimize the generic idle loop"
    This reverts commit 36c8994bcbb0f2499d2a6fd91adffaab5fd958b3.

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -212,8 +212,6 @@ DEFINE_PER_CPU(bool, cpu_dead_idle);
  */
 static void cpu_idle_loop(void)
 {
-	int cpu = smp_processor_id();
-
 	while (1) {
 		/*
 		 * If the arch has a polling bit, we maintain an invariant:
@@ -231,7 +229,7 @@ static void cpu_idle_loop(void)
 			check_pgt_cache();
 			rmb();
 
-			if (cpu_is_offline(cpu)) {
+			if (cpu_is_offline(smp_processor_id())) {
 				rcu_cpu_notify(NULL, CPU_DYING_IDLE,
 					       (void *)(long)smp_processor_id());
 				smp_mb(); /* all activity before dead. */
