762cb1c9f940c3cb7a93463655b0ec78da2d0427
Tyler Nijmeh <tylernij@gmail.com>
2020-06-15 09:10:02 -0700

    block: Queue requests on their origin CPU
    It has been tested that by using this flag, some benchmarks have
    reported a 20%-40% improvement due to cache hits.
    
    https://lwn.net/Articles/303270/
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -502,10 +502,12 @@ struct request_queue {
 #define QUEUE_FLAG_FAST        23	/* fast block device (e.g. ram based) */
 
 #define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_STACKABLE)	|	\
-				 (1 << QUEUE_FLAG_SAME_COMP))
+				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
+				 (1 << QUEUE_FLAG_SAME_FORCE))
 
 #define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_STACKABLE)	|	\
-				 (1 << QUEUE_FLAG_SAME_COMP))
+				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
+				 (1 << QUEUE_FLAG_SAME_FORCE))
 
 static inline void queue_lockdep_assert_held(struct request_queue *q)
 {
