1e6225329d50059ec924bd6566d2e0a3a3507d97
Edrick Vince Sinsuan <sedrickvince@gmail.com>
2020-07-19 14:28:03 +0300

    cpufreq: schedutil: Fix indentions
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -818,9 +818,9 @@ static int sugov_init(struct cpufreq_policy *policy)
 		tunables->down_rate_limit_us = 20000;
 		lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
 		if (lat) {
-                        tunables->up_rate_limit_us *= lat;
-                        tunables->down_rate_limit_us *= lat;
-                }
+			tunables->up_rate_limit_us *= lat;
+			tunables->down_rate_limit_us *= lat;
+		}
 	}
 
 	if (task_is_booster(current))
