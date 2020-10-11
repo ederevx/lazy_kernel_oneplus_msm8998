9697d5ca87248ff1999a6867aec3b6352b051d9f
Edrick Vince Sinsuan <sedrickvince@gmail.com>
2020-10-11 22:12:54 +0300

    sched: Separate boosting utilization from biasing through creating a new tunable schedtune.boost_bias
    Create a new tunable to separately control task cpu biasing from boosting task utilization. This will give us more control when it comes to manipulating scheduler's behavior in order to overall improve efficiency. This way, we no longer need to assign a specific value to be excluded in order to just disable utilization margining.
    
    Let's also take this opportunity to remove unnecessary crucial macro and also fix some spaces.
    
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- kernel/sched/tune.h
+++ kernel/sched/tune.h
@@ -16,8 +16,9 @@ struct target_nrg {
 
 int schedtune_cpu_boost(int cpu);
 int schedtune_task_boost(struct task_struct *tsk);
-int schedtune_task_boost_rcu_locked(struct task_struct *tsk);
 
+int schedtune_boost_bias_rcu_locked(struct task_struct *tsk);
+int schedtune_boost_bias(struct task_struct *tsk);
 int schedtune_prefer_idle(struct task_struct *tsk);
 
 void schedtune_exit_task(struct task_struct *tsk);
@@ -45,6 +46,7 @@ int schedtune_accept_deltas(int nrg_delta, int cap_delta,
 
 #define schedtune_cpu_boost(cpu)  0
 #define schedtune_task_boost(tsk) 0
+#define schedtune_boost_bias(tsk)    0
 
 #define schedtune_exit_task(task) do { } while (0)
 
