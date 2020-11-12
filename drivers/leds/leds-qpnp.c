be6696f754a3051e718ac0690e51d323bcf60b91
Edrick Vince Sinsuan <sedrickvince@gmail.com>
2020-11-12 13:48:16 +0300

    leds-qpnp: Set led_speed to 2
    As per user request, let's set it to 2.
    
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- drivers/leds/leds-qpnp.c
+++ drivers/leds/leds-qpnp.c
@@ -269,7 +269,7 @@ static u8	shutdown_enable = 0;
 
 int led_enable_fade = 1;	// default is fading
 int led_intensity = 0;		// default is stock intensity
-int led_speed = 0;			// default is stock speed
+int led_speed = 2;	// set to 2
 
 u32 convert_pause_hi_store (u32 value)
 {
