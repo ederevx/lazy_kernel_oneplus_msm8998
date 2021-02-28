c8992512a40d0d9f8f247c150eb221e81c228365
Edrick Vince Sinsuan <sedrickvince@gmail.com>
2021-02-27 19:31:16 -0600

    dynamic_schedtune: Improve dynamic schedtune thread consistency
    Acquire update directly at MDSS FB Atomic commit function to also accomodate for updates from MDSS compat as well as make it more accurate. Remove limitation in KGSL ioctl, instead make it acquire a dynamic schedtune update as well for each GPU command to ensure a more instantaneous update between inactive and active periods.
    
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -3786,6 +3786,10 @@ int mdss_fb_atomic_commit(struct fb_info *info,
 		goto end;
 	}
 
+#ifdef CONFIG_DYNAMIC_STUNE
+	dynstune_acquire_update();
+#endif
+
 	commit_v1 = &commit->commit_v1;
 	if (commit_v1->flags & MDP_VALIDATE_LAYER) {
 		ret = mdss_fb_wait_for_kickoff(mfd);
@@ -5413,9 +5417,6 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
-#ifdef CONFIG_DYNAMIC_STUNE
-		dynstune_acquire_update();
-#endif
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
