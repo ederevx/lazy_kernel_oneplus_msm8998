dbde664fcb8af00e38d30a44a7264e52d5a670db
Edrick Vince Sinsuan <sedrickvince@gmail.com>
2021-03-02 22:01:11 -0600

    cpufreq: schedutil: Nuke IOWAIT
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -3569,7 +3569,6 @@ static inline unsigned long rlimit_max(unsigned int limit)
 
 #define SCHED_CPUFREQ_RT        (1U << 0)
 #define SCHED_CPUFREQ_DL        (1U << 1)
-#define SCHED_CPUFREQ_IOWAIT    (1U << 2)
 
 #ifdef CONFIG_CPU_FREQ
 struct update_util_data {
