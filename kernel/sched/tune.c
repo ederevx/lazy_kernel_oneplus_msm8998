d03e7db228b100bf34c9a9dda2ed39e902fcac26
ederekun <sedrickvince@gmail.com>
2021-09-22 16:04:08 +0300

    sched/tune: Reconfigure schedtune parameters accordingly
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -786,9 +786,9 @@ static void write_default_values(struct cgroup_subsys_state *css)
 	static struct st_data st_targets[] = {
 		{ "audio-app",	0, 0, 0 },
 		{ "background",	0, 0, 0 },
-		{ "foreground",	1, 0, 0 },
+		{ "foreground",	0, 1, 0 },
 		{ "rt",		0, 0, 0 },
-		{ "top-app",	1, 0, 1 }
+		{ "top-app",	25, 1, 1 }
 	};
 	int i;
 
